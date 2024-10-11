bb7a5d4ba5bd5bb16826818dc6e0216dd740950b
Enderson Quiroz <endequirozca@cesde.net>
2024-10-10 20:55:45 -0500

    introduccion al Dom y objectos de js

---
+++ js/validator.js
@@ -0,0 +1,13 @@
+// console.log(document.getElementById('fecha'))
+// console.log(document.getElementsByClassName('clase'))
+// console.log(document.getElementsByTagName('input'))
+
+// console.log(document.querySelector('input'))
+// console.log(document.querySelectorAll('input'))
+// console.log(document.querySelector('#fecha')) /*permite seleccionar clases tipo css*/
+// console.log(document.querySelectorAll('#fecha')) /* permite seleccionar todo lo que tenga el item*/
+// console.log(document.querySelector('.clase'))
+// console.log(document.querySelectorAll('.clase'))
+
+let input = document.getElementById('fecha')
+console.log(input)
