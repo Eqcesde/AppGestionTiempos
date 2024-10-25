fcd7836d09b99106433a422b63ae99a5432fb251
Enderson Quiroz <endequirozca@cesde.net>
2024-10-24 20:37:19 -0500

    se crea extructura de vista de view proyectos

---
+++ Controllers/ControllerProyectos.js
@@ -0,0 +1,18 @@
+import { proyectos } from "../Models/ModelProyectos.js";
+
+export function listarProyectos() {
+
+}
+
+export function filtrarPorEmpresa() {
+
+}
+
+export function filtarPorProyecto(){
+
+}
+
+export function filtrarPorTipoProyectos(){
+
+
+}
